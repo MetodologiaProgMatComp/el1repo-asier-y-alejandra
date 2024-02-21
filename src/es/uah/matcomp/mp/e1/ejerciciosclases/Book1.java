8f0273d38a787ee6acbaa1dea7fbad7de17ea04d
alejandrainiesta <alejandra.iniesta@edu.uah.es>
2024-02-21 13:43:49 +0100

    2.1 Author y Book (acabado)

--- src/es/uah/matcomp/mp/e1/ejerciciosclases/Book1.java
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/Book1.java
@@ -40,40 +40,9 @@ public class Book1 {
         return "Book[name=" + name + ",author=" + author +",price=" + price + ",qty=" + qty + "]";
     }
 
-    //TEST
-    public static void main(String[]args) {
-        // Construct an author instance
-        Author1 ahTeck = new Author1("Tan Ah Teck", "ahteck@nowhere.com", 'm');
-        System.out.println(ahTeck); // Author's toString()
-
-        Book1 dummyBook1 = new Book1("Java for dummy", ahTeck, 19.95, 99); // Test Book's Constructor
-        System.out.println(dummyBook1); // Test Book's toString()
-
-        // Test Getters and Setters
-        dummyBook1.setPrice(29.95);
-        dummyBook1.setQty(28);
-        System.out.println("name is: " + dummyBook1.getName());
-        System.out.println("price is: " + dummyBook1.getPrice());
-        System.out.println("qty is: " + dummyBook1.getQty());
-
-        System.out.println("Author is: " + dummyBook1.getAuthor()); // Author's toString()
-
-        System.out.println("Author's name is: " + dummyBook1.getAuthor().getName());
-        System.out.println("Author's email is: " + dummyBook1.getAuthor().getEmail());
-
-        // Use an anonymous instance of Author to construct a Book instance
-        Book1 anotherBook = new Book1("more Java",
-                new Author1("Paul Tan", "paul@somewhere.com", 'm'), 29.95);
-        System.out.println(anotherBook); // toString()
-        System.out.println("Author's name is: " + dummyBook1.getAuthor().getName());
-        System.out.println("Author's email is: " + dummyBook1.getAuthor().getEmail());
-    }
 
     //MODIFICACIONES
-
-
     ///APARTADO 2
-
     //Nuevo método para obtener el nombre del autor
     public String getAuthor1Name() {
         return author.getName(); // cannot use author.name as name is private in Author class
