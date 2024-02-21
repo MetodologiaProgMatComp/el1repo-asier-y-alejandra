bd6f27af618ef347400625071781e66007d1ab4c
Asier2705 <asier.guerra@edu.uah.es>
2024-02-21 11:30:02 +0100

    test ball acabado

--- test/java/es/uah/matcomp/mp/e1/ejerciciosclases/BallTest.java
+++ test/java/es/uah/matcomp/mp/e1/ejerciciosclases/BallTest.java
@@ -8,96 +8,96 @@ class BallTest {
 
     @Test
     void getX() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
-        assertEquals(4.0, ball.getX());
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
+        assertEquals(4.0f, ball.getX());
     }
 
     @Test
     void setX() {
-        Ball ball = new Ball (4.0, 6.0, 2, -1.0, 3.0);
-        ball.setX(6.0);
-        assertEquals(6.0, ball.getX());
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
+        ball.setX(6.0f);
+        assertEquals(6.0f, ball.getX());
     }
 
     @Test
     void getY() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
-        assertEquals(6.0, ball.getY());
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
+        assertEquals(6.0f, ball.getY());
     }
 
     @Test
     void setY() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
-        ball.setY(8.0);
-        assertEquals(8.0, ball.getY());
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
+        ball.setY(8.0f);
+        assertEquals(8.0f, ball.getY());
     }
 
     @Test
     void getRadius() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
         assertEquals(2, ball.getRadius());
     }
 
     @Test
     void setRadius() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
         ball.setRadius(4);
         assertEquals(4, ball.getRadius());
     }
 
     @Test
     void getXDelta() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
-        assertEquals(-1.0, ball.getXDelta());
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
+        assertEquals(-1.0f, ball.getXDelta());
     }
 
     @Test
     void setXDelta() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
-        ball.setXDelta(-3.0);
-        assertEquals(-3.0, ball.getXDelta());
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
+        ball.setXDelta(-3.0f);
+        assertEquals(-3.0f, ball.getXDelta());
     }
 
     @Test
     void getYDelta() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
-        assertEquals(3.0, ball.getYDelta());
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
+        assertEquals(3.0f, ball.getYDelta());
     }
 
     @Test
     void setYDelta() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
-        ball.setYDelta(3.2);
-        assertEquals(3.2, ball.getYDelta());
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
+        ball.setYDelta(3.2f);
+        assertEquals(3.2f, ball.getYDelta());
     }
 
     @Test
     void move() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
         ball.move();
-        assertEquals(3.0, ball.getX());
-        assertEquals(9.0, ball.getY());
+        assertEquals(3.0f, ball.getX());
+        assertEquals(9.0f, ball.getY());
     }
 
     @Test
     void reflectHorizontal() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
         ball.reflectHorizontal();
-        assertEquals(1.0, ball.getXDelta());
-        assertEquals(3.0, ball.getYDelta());
+        assertEquals(1.0f, ball.getXDelta());
+        assertEquals(3.0f, ball.getYDelta());
     }
 
     @Test
     void reflectVertical() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
         ball.reflectVertical();
-        assertEquals(-1.0, ball.getXDelta());
-        assertEquals(-3.0, ball.getYDelta());
+        assertEquals(-1.0f, ball.getXDelta());
+        assertEquals(-3.0f, ball.getYDelta());
     }
 
     @Test
     void testToString() {
-        Ball ball = new Ball(4.0, 6.0, 2, -1.0, 3.0);
-        assertEquals("Ball[(4.0,6.0), speed=(-1.0, 3.0)]", ball.toString());
+        Ball ball = new Ball(4.0f, 6.0f, 2, -1.0f, 3.0f);
+        assertEquals("Ball[(4.0,6.0),speed=(-1.0,3.0)]", ball.toString());
     }
 }
